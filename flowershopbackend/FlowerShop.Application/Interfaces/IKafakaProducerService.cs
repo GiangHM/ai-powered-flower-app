14b78e518251d38736a362804d43b21c5cf89f44
GiangHM <36400582+GiangHM@users.noreply.github.com>
2026-05-03 15:29:26 +0700

    Delete unused code

--- flowershopbackend/FlowerShop.Application/Interfaces/IKafakaProducerService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Confluent.Kafka;
-
-namespace FlowerShop.Application.Interfaces
-{
-    public interface IKafakaProducerService<TKey, TValue>
-    {
-        Task<bool> ProduceAsync(string topic, TKey key, TValue value);
-    }
-}
